10031ffb5cdb6a73dd2430794af9b50b6653e2c3
s10w <slow3195@naver.com>
2019-08-13 21:51:48 +0900

    Player Move v1.0
    캐릭터 이동 제작

---
+++ TPP-Defence-Project/Assets/Script/camera_move.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 85.0f;
+    private const float DISTANCE_MIN = 2.0f;
+    private const float DISTANCE_MAX = 15.0f;
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 15.0f;
+    private float currentX = 0.0f;
+    private float currentY = 85.0f;
+    private float sensivityX = 4.0f;
+    private float sensivityY = 1.0f;
+
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            currentX += (2 * Input.GetAxis("Mouse X"));
+            currentY += (2 * Input.GetAxis("Mouse Y"));
+        }
+
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+    private void LateUpdate()
+    {
+        Vector3 Dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * Dir;
+        camTransform.LookAt(lookAt.position);
+
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            distance += 1;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            distance -= 1;
+        }
+        distance = Mathf.Clamp(distance, DISTANCE_MIN, DISTANCE_MAX);
+    }
+}
---
+++ TPP-Defence-Project/Assets/Script/player_move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Hit");
+
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100);
+
+            agent.SetDestination(hit.point);
+        }
+    }
+}
