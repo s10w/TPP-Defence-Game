d9fd289face02aa557bfd297788ca134fac90af8
s10w <slow3195@naver.com>
2019-08-16 23:58:22 +0900

    Camera Move v1.0
    카메라 이동 구현 (줌 기능 x)

---
+++ TPP-Defence-Project/Assets/Script/Camera_Ctrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Ctrl : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 camera_distance;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        transform.rotation = Quaternion.Euler(70f, 140f, 0f);
+
+        camera_distance = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + camera_distance, 2f);
+    }
+}
---
+++ TPP-Defence-Project/Assets/Script/Player_ctrl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Player_ctrl : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Vector3 target_pos;
+
+    Animator anim;
+
+    private bool move_state = false;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Player_move();
+        Player_motion();
+    }
+
+    void Player_move()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            move_state = true;
+
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100);
+            target_pos = hit.point;
+
+            agent.SetDestination(hit.point);
+        }
+
+        float target_distance = Vector3.Distance(transform.position, target_pos);
+        if (target_distance < 0.5f)
+        {
+            move_state = false;
+        }
+    }
+
+    void Player_motion()
+    {
+        if (move_state == true)
+        {
+            anim.SetInteger("motion", 1);
+        }
+        else
+        {
+            anim.SetInteger("motion", 0);
+        }
+    }
+}
