8fcc24c4c409787bbc96f91c78977db99114a9f3
s10w <slow3195@naver.com>
2019-08-23 17:09:34 +0900

    Project v1.01
    플레이어 공격 완성

--- TPP-Defence-Project/Assets/Script/Player_ctrl.cs
+++ TPP-Defence-Project/Assets/Script/Player_ctrl.cs
@@ -10,11 +10,9 @@ public class Player_ctrl : MonoBehaviour
     private Animator anim;
     private RaycastHit hit;
     private Vector3 target_pos;
-    public GameObject target_check;
-
     private Coroutine attack_Loop;
 
-    public bool move_state { get; private set; }
+    [SerializeField] private GameObject target_check;
     public bool attack_state { get; private set; }
 
     // Player_ctrl의 변수 초기화
@@ -23,7 +21,6 @@ public class Player_ctrl : MonoBehaviour
         target_check = Instantiate(target_check);
         target_check.SetActive(false);
 
-        move_state = false;
         attack_state = false;
     }
 
@@ -37,54 +34,73 @@ public class Player_ctrl : MonoBehaviour
     }
 
     // 매 프레임마다 호출
-    void Update()
+    void FixedUpdate()
     {
-        // 적 오브젝트가 있는 지 확인 
-        if (Input.GetMouseButton(1))
+        if (target_check.GetComponent<Target_check>().enemy_check == true)
         {
+            Player_attack();
+        }
+        else
+        {
+            Player_stop();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            // 적 오브젝트가 있는 지 확인
             target_check.SetActive(true);
             Physics.Raycast(main_camera.ScreenPointToRay(Input.mousePosition), out hit, 100);
             target_check.transform.position = new Vector3(hit.point.x, 0f, hit.point.z);
-        }
 
-        if (target_check.GetComponent<Target_check>().enemy_check == true)
-        {
-            attack_state = true;
-            anim.SetInteger("motion", 2);
-            attack_Loop = StartCoroutine(Player_attack());
-        }
-
-        if (target_check.GetComponent<Target_check>().enemy_check == false)
-        {
-            if (attack_state == true)
-            {
-                StopCoroutine(attack_Loop);
-            }
-            attack_state = false;
+            //플레이어 이동
             Player_move();
         }
     }
 
-    // 플레이어 이동
+    // 플레이어 이동 함수
     void Player_move()
     {
-        if (Input.GetMouseButton(1))
+        // 이동 시작 시 공격 중지
+        if (attack_state == true)
         {
-            move_state = true;
-            anim.SetInteger("motion", 1);
-
-            target_pos = hit.point;
-            agent.SetDestination(target_pos);
+            StopCoroutine(attack_Loop);
         }
+        attack_state = false;
+
+        anim.SetInteger("motion", 1);
+        target_pos = hit.point;
+        agent.SetDestination(target_pos);
+    }
+
+    // 플레이어 정지 함수
+    void Player_stop()
+    {
+        // 목적지에 도달시 정지 모션
         float target_distance = Vector3.Distance(transform.position, target_pos);
         if (target_distance < 0.5f)
         {
-            move_state = false;
             anim.SetInteger("motion", 0);
         }
     }
 
-    IEnumerator Player_attack()
+    // 플레이어 공격 함수
+    void Player_attack()
+    {
+        //적 위치에 맞게 다시 목적지 설정
+        agent.SetDestination(target_check.GetComponent<Target_check>().Enemy_pos + (-transform.forward.normalized * 1.5f)); // 사정거리
+
+        // 사정거리에 도달 시 공격 시작
+        float Enemy_distance = Vector3.Distance(transform.position, target_check.GetComponent<Target_check>().Enemy_pos);
+        if (Enemy_distance < 3f)
+        {
+            attack_state = true;
+            anim.SetInteger("motion", 2);
+            attack_Loop = StartCoroutine(Create_hit());
+        }
+    }
+
+    // 공격 오브젝트 생성 함수
+    IEnumerator Create_hit()
     {
         Debug.Log("attack");
         yield return new WaitForSeconds(1f);
