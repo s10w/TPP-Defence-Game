a9266ce3e9311fc4cab2d1c5439c4c399a539fcf
SoonHo-Yoon <52959848+SoonHo-Yoon@users.noreply.github.com>
2019-08-28 18:02:10 +0900

    Project v1.03
    카메라 시점변환 및 이동

--- TPP-Defence-Project/Assets/Script/Camera_Ctrl.cs
+++ TPP-Defence-Project/Assets/Script/Camera_Ctrl.cs
@@ -2,22 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Camera_Ctrl : MonoBehaviour
 {
+    private float distance = 15.0f;
+    private float current_x = 135.0f;
+    private float current_y = 70.0f;
+    private float edge_size = 30f;
+
+
+    private bool camera_ctrl_state = false;
+
     private GameObject player;
-    private Vector3 camera_distance;
+
+    private Transform cam;
+
+
+
+
 
     void Start()
     {
-        //transform.position = new Vector3(140f, 10f, 145f);
-        transform.rotation = Quaternion.Euler(70f, 140f, 0f);
-
         player = GameObject.FindGameObjectWithTag("Player");
-        camera_distance = transform.position - player.transform.position;
+        cam = GetComponent<Transform>();
     }
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, player.transform.position + camera_distance, 2f);
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            camera_ctrl_state = false;
+        }
+        if (current_x == 140.0f && current_y == 70.0f){
+            if (Input.mousePosition.y > Screen.height - edge_size || Input.mousePosition.y < edge_size ||
+                Input.mousePosition.x > Screen.width - edge_size || Input.mousePosition.x < edge_size)
+            {
+                camera_ctrl_state = true;
+            }
+        }
+        if (camera_ctrl_state == true)
+        {
+            Cam_ctrl();
+        }
+
+        if (camera_ctrl_state == false)
+        {
+            Cam_unit_ctrl();
+        }
+
+    }
+    void Cam_ctrl()
+    {
+        float movement = 10f;
+
+        Vector3 campos = cam.position;
+
+        if (Input.mousePosition.y > Screen.height - edge_size)
+        {
+            campos.x += movement * Time.deltaTime;
+            campos.z -= movement * Time.deltaTime;
+        }
+        if (Input.mousePosition.y < edge_size)
+        {
+            campos.x -= movement * Time.deltaTime;
+            campos.z += movement * Time.deltaTime;
+        }
+        if (Input.mousePosition.x > Screen.width - edge_size)
+        {
+            campos.x -= movement * Time.deltaTime;
+            campos.z -= movement * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < edge_size)
+        {
+            campos.x += movement * Time.deltaTime;
+            campos.z += movement * Time.deltaTime;
+        }
+
+        cam.position = campos;
+
+    }
+    void Cam_unit_ctrl()
+    {
+        float Y_ANGLE_MIN = 1.0f;
+        float Y_ANGLE_MAX = 80.0f;
+        float DISTANCE_MIN = 3.0f;
+        float DISTANCE_MAX = 18.0f;
+
+        // 카메라 좌우회전
+        if (Input.GetMouseButton(2))
+        {
+            current_x += (2 * Input.GetAxis("Mouse X"));
+            current_y += -(2 * Input.GetAxis("Mouse Y"));
+        }
+        current_y = Mathf.Clamp(current_y, Y_ANGLE_MIN, Y_ANGLE_MAX);
+
+        Vector3 Dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(current_y, current_x, 0);
+
+        // 카메라 위치 이동
+        cam.position = player.transform.position + player.transform.up * 2 + rotation * Dir;
+        cam.LookAt(player.transform.position + player.transform.up * 2);
+
+        // 카메라 상하
+
+        distance -= Input.GetAxis("Mouse ScrollWheel") * 100f * Time.deltaTime;
+        distance = Mathf.Clamp(distance, DISTANCE_MIN, DISTANCE_MAX);
+
+        // 카메라 위치 초기화
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            current_x = 140.0f;
+            current_y = 70.0f;
+            distance = 15.0f;
+        }
     }
 }
